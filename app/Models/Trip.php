16ab7e4a6c94d1bef066eb751ff7ecd16ca9b002
adelia '- <adeliaswastikadewi@gmail.com>
2025-11-23 01:00:31 +0700

    feat: create base blade layout with tailwind css (#5)

--- app/Models/Trip.php
+++ app/Models/Trip.php
@@ -9,7 +9,7 @@ class Trip extends Model
 {
     use HasFactory;
 
-    // Kolom yang diizinkan untuk diisi (digunakan oleh P3 dan P1)
+    //Kolom yang diizinkan untuk diisi (digunakan oleh P3 dan P1)
     protected $fillable = [
         'user_id', 
         'driver_id', 
@@ -20,17 +20,17 @@ class Trip extends Model
         'total_cost'
     ];
     
-    // Relasi ke Penumpang (User)
+    //Relasi ke Penumpang (User)
     public function passenger()
     {
-        // Trip belongs to a User (sebagai Passenger)
+        //Trip belongs to a User (sebagai Passenger)
         return $this->belongsTo(User::class, 'user_id');
     }
 
-    // Relasi ke Driver
+    //Relasi ke Driver
     public function driver()
     {
-        // Trip belongs to a Driver
+        //Trip belongs to a Driver
         return $this->belongsTo(Driver::class, 'driver_id');
     }
 }
