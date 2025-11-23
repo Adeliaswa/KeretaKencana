e2eb6d0bc542a581f907cca0fe4b5c31e997364b
nadhifa <nadhifafitriyaah@gmail.com>
2025-11-23 13:48:21 +0700

    feat: add passenger booking and history routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 use App\Http\Controllers\Passenger\TripController;
 
-Route::middleware(['auth', 'role:passenger'])->group(function() {
-    Route::get('/booking', [TripController::class, 'create'])->name('passenger.booking');
-    Route::post('/booking', [TripController::class, 'store'])->name('passenger.booking.store');
-});
+Route::middleware(['auth', 'role:passenger'])
+    ->prefix('passenger')
+    ->name('passenger.')
+    ->group(function () {
+
+        Route::get('/booking', [TripController::class, 'index'])->name('booking');
+        Route::post('/booking', [TripController::class, 'store'])->name('booking.store');
+        Route::get('/history', [TripController::class, 'history'])->name('history');
+    });
